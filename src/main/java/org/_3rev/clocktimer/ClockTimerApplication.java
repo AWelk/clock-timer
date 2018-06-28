2b1f28b79339a9ca8396055e83fd6a070684287a
Alex Welk <alexwelk.92@gmail.com>
2018-06-27 21:50:30 -0400

    Initial commit

---
+++ src/main/java/org/_3rev/clocktimer/ClockTimerApplication.java
@@ -0,0 +1,12 @@
+package org._3rev.clocktimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockTimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClockTimerApplication.class, args);
+    }
+}
---
+++ src/test/java/org/_3rev/clocktimer/ClockTimerApplicationTests.java
@@ -0,0 +1,16 @@
+package org._3rev.clocktimer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClockTimerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
